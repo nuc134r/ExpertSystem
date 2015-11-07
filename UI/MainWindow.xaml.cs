94165a5841f1b3f5ac9935eb6ca87fd315a0bf3a
nuc134r <chelman.soft@gmail.com>
2015-11-07 17:41:17 +0300

    Анимация при запуске программы

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -21,13 +24,28 @@ namespace UI
     public partial class MainWindow : Window
     {
         private bool isRunning;
+        private SolidColorBrush brush;
 
+        // ReSharper disable once PossibleNullReferenceException
         private static readonly Color RunningColor = (Color)ColorConverter.ConvertFromString("#CA5100");
         private static readonly Color ReadyColor = SystemColors.MenuHighlightColor;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            InitailizeAnimationBrush();
+        }
+
+        private void InitailizeAnimationBrush()
+        {
+            brush = new SolidColorBrush();
+
+            statusBar.Background = brush;
+            LaunchStopBox.Background = brush;
+
+            brush.Color = ReadyColor;
+            RegisterName("brush", brush);
         }
 
         private void SourceCodeBox_OnScrollChanged(object sender, ScrollChangedEventArgs e)
@@ -55,19 +73,40 @@ namespace UI
             }
         }
 
+        private void AnimateModeChange(ApplicationMode mode)
+        {
+            var fromColor = mode == ApplicationMode.Ready ? ReadyColor : RunningColor;
+            var toColor = mode == ApplicationMode.Ready ? RunningColor : ReadyColor;
+
+            var animation = new ColorAnimation
+            {
+                From = fromColor,
+                To = toColor,
+                Duration = TimeSpan.FromMilliseconds(175)
+            };
+
+            Storyboard.SetTargetName(animation, "brush");
+            Storyboard.SetTargetProperty(animation, new PropertyPath(SolidColorBrush.ColorProperty));
+
+            var sb = new Storyboard();
+            sb.Children.Add(animation);
+
+            sb.Begin(this);
+        }
+
         private void LaunchStopButton_MouseDown(object sender, MouseButtonEventArgs e)
         {
             isRunning = !isRunning;
 
             if (isRunning)
             {
-                statusBar.Background = LaunchStopBox.Background = new SolidColorBrush(RunningColor);
+                AnimateModeChange(ApplicationMode.Ready);
                 LaunchStopButton.Content = "  Стоп";
                 statusBarInfo.Content = "Отладка";
             }
             else
             {
-                statusBar.Background = LaunchStopBox.Background = new SolidColorBrush(ReadyColor);
+                AnimateModeChange(ApplicationMode.Running);
                 LaunchStopButton.Content = "Запуск";
                 statusBarInfo.Content = "Готов";
             }
