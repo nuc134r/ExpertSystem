a0826d414f54e4b353c0b47c7a2b48885a13b01b
nuc134r <chelman.soft@gmail.com>
2015-11-21 21:08:00 +0300

    Тесты на успешный парсинг Фактов

---
+++ Tests/WhenParsing/DoesNotThrow.cs
@@ -0,0 +1,25 @@
+﻿using Core;
+using NUnit.Framework;
+
+namespace Tests.WhenParsing
+{
+    [TestFixture]
+    public class DoesNotThrow
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            parser = new Parser();
+        }
+
+        private Parser parser;
+
+        [Test]
+        public void DoesNotThrowOnEmptyLine()
+        {
+            const string code = "";
+
+            Assert.DoesNotThrow(() => { parser.Do(code, new RunContext()); });
+        }
+    }
+}
---
+++ Tests/WhenParsing/SuccessfullyParsesFact.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Core;
+using NUnit.Framework;
+
+namespace Tests.WhenParsing
+{
+    [TestFixture]
+    public class SuccessfullyParsesFact
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            parser = new Parser();
+        }
+
+        private Parser parser;
+
+        [Test]
+        public void WithOneArgument()
+        {
+            const string code = "Human(Mark);";
+
+            var context = new RunContext();
+            parser.Do(code, context);
+
+            var fact = context.Facts.First();
+
+            Assert.AreEqual("Human", fact.Name);
+            Assert.AreEqual("Mark", fact.Arguments.First().Name);
+        }
+
+        [Test]
+        public void WithTwoArguments()
+        {
+            const string code = "Likes(Alex, Miranda);";
+
+            var context = new RunContext();
+            parser.Do(code, context);
+
+            var fact = context.Facts.First();
+
+            Assert.AreEqual("Likes", fact.Name);
+            Assert.AreEqual("Alex", fact.Arguments.First().Name);
+            Assert.AreEqual("Miranda", fact.Arguments.Last().Name);
+        }
+    }
+}
