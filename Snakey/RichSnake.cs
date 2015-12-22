80a4281335b8ec34cf10b86b822c9ac8def32990
nuc134r <chelman.soft@gmail.com>
2015-12-23 02:16:18 +0300

    Чистка проекта

--- Snakey/RichSnake.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace EasterEggs
-{
-    
-}
---
+++ Version.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("2.0.0.0")]
+
+// 1.10.0.*     Added MSbuild process for CI (GitHub, TeamCity.CodeBetter.com, NuGet.org)
